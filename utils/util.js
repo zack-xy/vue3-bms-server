660652f316be38caa7a59a64ed81ccae9d2f9cb6
zack zheng <zack_zhengxiyun@163.com>
2021-11-07 23:21:27 +0800

    no message

--- utils/util.js
+++ utils/util.js
@@ -4,11 +4,11 @@
 const log4j = require('log4js')
 const CODE = {
   SUCCESS: 200,
-  PARAM_ERROR: 101, // 参数错误
-  USER_ACCOUNT_ERROR: 201, // 账号或密码错误
-  USER_LOGIN_ERROR: 301, // 用户未登陆
-  BUSINESS_ERROR: 401, //业务请求失败
-  AUTH_ERROR: 501, //认证失败或者token过期
+  PARAM_ERROR: 1001, // 参数错误
+  USER_ACCOUNT_ERROR: 2001, // 账号或密码错误
+  USER_LOGIN_ERROR: 3001, // 用户未登陆
+  BUSINESS_ERROR: 4001, //业务请求失败
+  AUTH_ERROR: 5001, //认证失败或者token过期
 }
 module.exports = {
   /**
@@ -35,7 +35,7 @@ module.exports = {
       code, data, msg
     }
   },
-  
+
   fail (msg = '', code = CODE.BUSINESS_ERROR) {
     log4j.debug(msg)
     return {
