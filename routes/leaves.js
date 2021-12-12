975e033721846b620edb3bd8d77f3fe109dd9a69
zack zheng <zack_zhengxiyun@163.com>
2021-12-12 21:14:19 +0800

    no message

--- routes/leaves.js
+++ routes/leaves.js
@@ -52,40 +52,44 @@ router.post('/operate', async (ctx) => {
   const { _id, action, ...params } = ctx.request.body
   let authorization = ctx.request.headers.authorization
   let { data } = util.decoded(authorization)
-  if (action === 'create') {
-    let orderNo = "XJ"
-    orderNo += util.formatDate(new Date(), 'yyyyMMdd')
-    const total = await Leave.countDocuments()
-    params.orderNo = orderNo + total
-    // 获取部门负责人信息
-    let id = data.deptId.pop()
-    let dept = await Dept.findById(id)
-    // 获取人事部门和财务部门
-    let userList = await Dept.find({ deptName: { $in: ['人事部门', '财务部门'] } })
-    let auditUsers = dept.userName
-    let auditFlows = [
-      { userId: dept.userId, userName: dept.userName, userEmail: dept.userEmail }
-    ]
-    userList.map(item => {
-      auditFlows.push(
-        { userId: item.userId, userName: item.userName, userEmail: item.userEmail }
-      )
-      auditUsers += ',' + item.userName
-    })
-    params.auditUsers = auditUsers
-    params.curAuditUserName = dept.userName
-    params.auditFlows = auditFlows
-    params.auditLogs = []
-    params.applyUser = {
-      userId: data.userId,
-      userName: data.userName,
-      userEmail: data.userEmail,
+  try {
+    if (action === 'create') {
+      let orderNo = "XJ"
+      orderNo += util.formatDate(new Date(), 'yyyyMMdd')
+      const total = await Leave.countDocuments()
+      params.orderNo = orderNo + total
+      // 获取部门负责人信息
+      let id = data.deptId.pop()
+      let dept = await Dept.findById(id)
+      // 获取人事部门和财务部门
+      let userList = await Dept.find({ deptName: { $in: ['人事部门', '财务部门'] } })
+      let auditUsers = dept.userName
+      let auditFlows = [
+        { userId: dept.userId, userName: dept.userName, userEmail: dept.userEmail }
+      ]
+      userList.map(item => {
+        auditFlows.push(
+          { userId: item.userId, userName: item.userName, userEmail: item.userEmail }
+        )
+        auditUsers += ',' + item.userName
+      })
+      params.auditUsers = auditUsers
+      params.curAuditUserName = dept.userName
+      params.auditFlows = auditFlows
+      params.auditLogs = []
+      params.applyUser = {
+        userId: data.userId,
+        userName: data.userName,
+        userEmail: data.userEmail,
+      }
+      let res = await Leave.create(params)
+      ctx.body = util.success(res, '创建成功')
+    } else {
+      let res = await Leave.findByIdAndUpdate(_id, { applyState: 5 })
+      ctx.body = util.success(res, '作废成功')
     }
-    let res = await Leave.create(params)
-    ctx.body = util.success(res, '创建成功')
-  } else {
-    let res = await Leave.findByIdAndUpdate(_id, { applyState: 5 })
-    ctx.body = util.success(res, '作废成功')
+  } catch (error) {
+    ctx.body = util.fail(`异常:${error.message}`)
   }
 })
 
